64ddcfc5bc0cf1feb368512d22a60e43ffd39a85
Minjee <grace_123@nate.com>
2024-10-19 23:15:31 +0900

    예제 5-2 연습(모든 HTTP 헤더 정보 값 출력하기)

---
+++ JSP_connected_with_gitHub/src/main/java/examples/exam05_2.java
@@ -0,0 +1,26 @@
+package examples;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/5_2")
+public class exam05_2 extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher ds = req.getRequestDispatcher("JSP_exam/request02.jsp");
+		ds.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
