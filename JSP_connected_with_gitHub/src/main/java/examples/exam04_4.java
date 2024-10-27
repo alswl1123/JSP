51c12c80aeee22e2e5512e82b55e7d90221db9a4
Minjee <grace_123@nate.com>
2024-10-27 22:56:44 +0900

    예제 4-4 연습(include 액션 태그, param 액션 태그, 제목/현재 날짜)

---
+++ JSP_connected_with_gitHub/src/main/java/examples/exam04_4.java
@@ -0,0 +1,26 @@
+package examples;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/4_4")
+public class exam04_4 extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher ds = req.getRequestDispatcher("JSP_exam/param02.jsp");
+		ds.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+}
