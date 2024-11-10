3e7df0942f31f58726e4c647a4761616779b6d61
Minjee <grace_123@nate.com>
2024-11-10 23:40:25 +0900

    SELECT문 연습

--- JSP_connected_with_gitHub/src/main/webapp/Exam_prac.sql
+++ JSP_connected_with_gitHub/src/main/webapp/Exam_prac.sql
@@ -72,6 +72,90 @@ ALTER TABLE 고객
 
 -- 여기까지가 첫번째 시험 연습
 
+-- 자료 입력함
+
+INSERT INTO 고객 VALUES ('apple', '정소화', 20,'gold','학생',1000);
+INSERT INTO 고객 VALUES ('banana', '김선우', 25,'vip','간호사',2500);
+INSERT INTO 고객 VALUES ('carrot', '고명석', 28,'gold','교사',4500);
+INSERT INTO 고객 VALUES ('orange', '김용욱', 22,'silver','학생',0);
+INSERT INTO 고객 VALUES ('melon', '성원용', 35,'gold','회사원',5000);
+INSERT INTO 고객 VALUES ('peach', '오형준', NULL,'silver','의사',300);
+INSERT INTO 고객 VALUES ('pear', '채광주', 31,'silver','회사원',500);
+
+INSERT INTO 제품 VALUES ('p01', '그냥만두', 5000, 4500,'대한식품');
+INSERT INTO 제품 VALUES ('p02', '매운쫄면', 2500, 5500,'민국푸드');
+INSERT INTO 제품 VALUES ('p03', '쿵떡파이', 3600, 2600,'한빛제과');
+INSERT INTO 제품 VALUES ('p04', '맛난초콜릿', 1250, 2500,'한빛제과');
+INSERT INTO 제품 VALUES ('p05', '얼큰라면', 2200, 1200,'대한식품');
+INSERT INTO 제품 VALUES ('p06', '통통우동', 1000, 1550,'민국푸드');
+INSERT INTO 제품 VALUES ('p07', '달콤비스킷', 1650, 1500,'한빛제과');
+
+INSERT INTO 주문 VALUES ('o01','apple','p03', 10,'서울시 마포구','2019-01-01');
+INSERT INTO 주문 VALUES ('o02','melon','p01', 5,'인천시 계양구','2019-01-10');
+INSERT INTO 주문 VALUES ('o03','banana','p06', 45,'경기도 부천시','2019-01-11');
+INSERT INTO 주문 VALUES ('o04','carrot','p02', 8,'부산시 금정구','2019-02-01');
+INSERT INTO 주문 VALUES ('o05','melon','p06', 36,'경기도 용인시','2019-02-20');
+INSERT INTO 주문 VALUES ('o06','banana','p01', 19,'충청북도 보은군','2019-03-02');
+INSERT INTO 주문 VALUES ('o07','apple','p03', 22,'서울시 영등포구','2019-03-15');
+INSERT INTO 주문 VALUES ('o08','pear','p02', 50,'강원도 춘천시','2019-04-10');
+INSERT INTO 주문 VALUES ('o09','banana','p04', 15,'전라남도 목포시','2019-04-11');
+INSERT INTO 주문 VALUES ('o10','carrot','p03', 20,'경기도 안양시','2019-05-22');
+
+SELECT * FROM 고객;
+SELECT * FROM 제품;
+SELECT * FROM 주문;
+
+-- SELECT문
+-- SELECT [ALL | DISTINCT] 속성_리스트
+-- FROM 테이블_리스트;
+
+-- 예제 7-10 (고객 테이블에서 고객아이디, 고객이름, 등급 속성 검색)
+SELECT 고객아이디, 고객이름, 등급 FROM 고객;
+
+-- 예제 7-11 (고객 테이블에 존재하는 모든 속성 검색)
+SELECT 고객아이디, 고객이름, 나이, 등급, 직업, 적립금 FROM 고객;
+SELECT * FROM 고객; -- 이렇게 작성해도 됨 (예제 7-12)
+
+-- 예제 7-13 (제품 테이블에서 제조업체 검색)
+SELECT 제조업체 FROM 제품;
+
+-- 예제 7-15 (제품 테이블에서 제조업체 속성을 중복없이 검색)
+SELECT DISTINCT 제조업체 FROM 제품;
+
+-- 예제 7-16 (제품 테이블에서 제품명과 단가를 검색하되, 단가를 가격이라는 새 이름으로 출력)
+SELECT 제품명, 단가 AS 가격 FROM 제품;
+
+-- 예제 7-17 (단가+500 = 조정단가)
+SELECT 제품명, 단가 + 500 AS "조정단가" FROM 제품;
+
+-- SELECT [ALL | DISTINCT] 속성_리스트
+-- FROM 테이블_리스트
+-- [WHERE 조건];
+
+-- 예제 7-18 (제품 테이블에서 한빛제과가 제조한 제품의 제품명, 재고량, 단가 검색)
+SELECT 제품명, 재고량, 단가 
+FROM 제품
+WHERE 제조업체 = '한빛제과';
+
+-- 예제 7-19 (주문 테이블에서 apple 고객이 15개 이상 주문한 주문제품, 수량, 주문일자 검색)
+SELECT 주문제품, 수량, 주문일자
+FROM 주문
+WHERE 주문고객 = 'apple' AND 수량 >= 15;
+
+-- 예제 7-20 (주문 테이블에서 apple 고객이 주문했거나 15개 이상 주문된 제품의 주문제품, 수량, 주문일자, 주문고객 검색)
+SELECT 주문제품, 수량, 주문일자, 주문고객
+FROM 주문
+WHERE 주문고객 = 'apple' OR 수량 >= 15;
+
+-- 예제 7-21 (제품 테이블에서 단가가 2000원 이상이면서 3000원 이하인 제품의 제품명, 단가, 제조업체 검색)
+SELECT 제품명, 단가, 제조업체
+FROM 제품
+WHERE 단가 >= 2000 AND 단가 <= 3000;
+
+
+
+
+
 
 
 
